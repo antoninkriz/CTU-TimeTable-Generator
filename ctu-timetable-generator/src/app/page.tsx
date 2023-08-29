6a5cf3c440d768681acba1abf392fbdee7ead598
Antonín Kříž <15214494+antoninkriz@users.noreply.github.com>
2023-08-29 13:38:54 +0200

    Improved landing page text

--- ctu-timetable-generator/src/app/page.tsx
+++ ctu-timetable-generator/src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Index() {
     <>
       <Container maxW="3xl">
         <Stack as={Box} py={{ base: 5, md: 10 }}>
-          <Heading fontWeight={600} fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }} lineHeight="110%" pb={10} textAlign="center">
+          <Heading fontWeight={600} fontSize={{ base: 'xl', sm: '2xl', md: '6xl' }} lineHeight="110%" pb={4} textAlign="center">
             Studuješ na{' '}
             <Text color="blue.500" fontWeight="900" as="span">
               ČVUT
@@ -35,44 +35,44 @@ export default function Index() {
             ? <Text>Vytvoř si svůj optimální rozvrh hodin!</Text>
           </Heading>
           <Stack as={Box} textAlign="center">
-            <Text>Najdi svůj optimální rozvrh bez složitého hledání vhodných paralelek, přednášek, cvičení a samotných předmětů!</Text>
-            <Text>Každého studenta již od pradávna jistě trápí stejná otázka:</Text>
+            <Text>Najdi svůj ideální rozvrh hodin bez otravného hledání vhodných paralelek tvých předmětů!</Text>
+          </Stack>
+          <Stack as={Box} textAlign="center" mt={4}>
+            <Text>Studenty již od pradávna jistě trápí stejná otázka:</Text>
             <Text color={yellow} fontWeight="600" as="span">
               „Jak nechodit do školy a zároveň si odnést titul?“
             </Text>
           </Stack>
           <Stack as={Box} textAlign="justify" py={8} pt={0}>
             <Text>
-              Mnoho studentů již odpvoěď hledalo, ne mnoho z nás ji ale našlo. Ať proto, že jsou <Text as="i">rok od roku línější a hloupější</Text>, či proto,
-              že chtějí pracovat. Však Ty je určitě musíš znáš stejně nejlíp.
+              Ať už tě odpověď na tuto otázku zajímá proto, že chceš se školou stíhat i práci a své koníčky, nebo jen proto, že
+              jsi <Text as="i">rok od roku hloupější a línější</Text>, tak jsi zde správně!
             </Text>
             <Text>
-              Bohužel nejspíš ani tato appka Ti tuhle otázku bohužel nezodpoví, ale pomůže Ti najít takové složení hodin, které si zvolíš, abys strávil/a ve
-              škole času co nejméně.
+              Tahle appka Ti tuhle otázku nejspíš sice nezodpoví, ale pomůže Ti najít takové složení hodin, respektive přednášek, cvičení a laboratoří,
+              které si zvolíš, abys strávil/a ve škole času co nejméně.
             </Text>
             <Heading textAlign="center" fontWeight={600} fontSize={{ base: 'md', sm: 'xl', md: '2xl' }} lineHeight="150%" color={yellow}>
               Jak?
             </Heading>
             <Text>
-              Stačí si vybrat ze seznamu předměty, které tě zajímají, zvolit, zda chceš chodit na přednášky, nebo cvičení, nebo i do laboratoří, a nechat tvůj
-              počítač magicky tyhle informace přechroupat. Během chvilky na tebe vyskočí všechny{' '}
-              <Text fontWeight="600" as="i">
-                bezkolizní
-              </Text>{' '}
-              rozvrhy, které splňují Tvé zadání.
+              Stačí si vybrat ze seznamu semestr a předměty, které Tě zajímají, zvolit, zda chceš chodit na jen na cvičení nebo i na přednášky. Během chvilky
+              na tebe vyskočí všechny <Text fontWeight="600" as="i">bezkolizní</Text> rozvrhy, které splňují Tvé zadání.
             </Text>
             <Text>
-              Nemusíš se nijak přihlašovat do{' '}
+              Neboj, nemusíš se nijak přihlašovat do{' '}
               <Text fontWeight="600" as="span">
                 KOS
               </Text>
-              u, seznam předmětů si appka hlídá sama a všechny výpočty probíhají přímo u tebe na počítači. Výhoda toho je, že nemusím platit za žádné servery,
-              nevýhoda je, že to může, v příapdě náročnějších výběrů, chvilku trvat.
+              u, data appka získává sama bez Tvých údajů. Nemusíš se tedy bát o svá data. Všechny výpočty probíhají pouze na tvém počítači a nikam se nic
+              neodesílá. Nikdo se nedozví, že Tě jeho přednášky netrápí. A já ušetřím za náklady na servery.
             </Text>
             <Text>
-              V mezičase se mi neboj poslat srdíčko
-              na <Text as="span" whiteSpace="nowrap"><Link href={GITHUB_URL} color={blueLink} fontWeight="600" target="_blank">GitHubu</Link> <Icon as={IoHeart} boxSize={4} color="red.500" /></Text>,
-              nebo koukni na <Link href={PERSONAL_URL} color={blueLink} fontWeight="600" target="_blank">můj web</Link> (moc tam toho není, ale tímhle odkazem si určitě zlepšuju SEO).
+              V mezičase se mi neboj kliknout na srdíčko na{' '}
+              <Text as="span" whiteSpace="nowrap">
+                <Link href={GITHUB_URL} color={blueLink} fontWeight="600" target="_blank">GitHubu</Link> <Icon as={IoHeart} boxSize={4} color="red.500" />
+              </Text>, nebo koukni na <Link href={PERSONAL_URL} color={blueLink} fontWeight="600" target="_blank">můj web</Link>. Moc tam toho asi nenajdeš, ale
+              tenhle odkaz mi určitě zlepší SEO.
             </Text>
           </Stack>
           <Stack direction="column" spacing={3} align="center" alignSelf="center" position="relative">
